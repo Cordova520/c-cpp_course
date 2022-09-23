6028bfc4f428fb845c9825028b97ac6e90aaa72c
Cordova520 <daniel_cordovag98@hotmail.com>
2022-09-22 22:55:29 -0500

    First Course assignment

---
+++ week_1/dr_p_mistake/dr_p_mistake.c
@@ -0,0 +1,14 @@
+/*
+    Dr. P's Mistake
+    The following program is suppose to write hello world onto the screen but it has syntax errors  - find and correct.
+    Daniel Cordova-Guzman
+    September 22, 2022
+*/
+
+#include<stdio.h>
+
+int main(void)
+{   
+    printf(" hello world\n");
+    return 0;
+}    
