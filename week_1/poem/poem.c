f011b578e73dae796f705dc2552b149bfe98a171
Cordova520 <daniel_cordovag98@hotmail.com>
2022-09-22 23:11:37 -0500

    Print a poem using printf

---
+++ week_1/poem/poem.c
@@ -0,0 +1,14 @@
+/*
+    Print a Poem
+    The following program is suppose to write hello world onto the screen but it has syntax errors  - find and correct.
+    Daniel Cordova-Guzman
+    September 22, 2022
+*/
+
+#include<stdio.h>
+
+int main(void)
+{   
+    printf("No mountain, nor sea,\nno thing of this world,\ncould keep us apart,\nbecause this is not,\nmy world...\n");
+    return 0;
+}    
