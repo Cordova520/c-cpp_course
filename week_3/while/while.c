1332b859ef557af609d4b0696a5c8a1c0ba890d2
Cordova520 <daniel_cordovag98@hotmail.com>
2022-10-18 22:54:24 -0500

    Script for example of how the while loop works

---
+++ week_3/while/while.c
@@ -0,0 +1,29 @@
+/*
+    Daniel Cordova-Guzman
+    While loop
+    October 18, 2022
+*/
+
+#include <stdio.h>
+int main(){
+    int blanks = 0, digits = 0, letters = 0, others = 0;
+    int c; // use int value of character
+    while ((c = getchar()) != EOF)
+    {
+        if (c == ' '){
+            ++blanks;
+        }
+        else if (c >= '0' && c <= '9'){
+            ++digits;
+        }
+        else if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z'){
+            ++letters;
+        }else{
+            ++others;
+        }
+    };
+    printf(" blanks = %d, digits = %d, letters = %d,",
+             blanks, digits, letters);
+    printf(" others = %d\n\n", others);
+    return 0;
+}
